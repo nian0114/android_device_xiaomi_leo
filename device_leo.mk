9522fcea4b4d8810e7fa35c606463105a6dc6008
nian0114 <268078545@qq.com>
2016-06-28 20:39:59 +0800

    device.mk: Set properties required for perfd
    Change-Id: I30d6a4a19fa951abff942cfc0fb81c383353e15e

--- device_leo.mk
+++ device_leo.mk
@@ -328,3 +328,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.ecc_hard_count=1 \
     persist.radio.ecc_hard_1=111,113,117,122,125,110,120,119
+
+# for perfd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min_freq_0=384000
+    ro.min_freq_4=384000
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
